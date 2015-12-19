a9463273b668fdc609c58758ffe0b99784d7fb44
Max White <maxjwhite22@gmail.com>
2015-12-19 12:28:49 -0500

    working section structs

--- tomad.ino
+++ tomad.ino
@@ -1,8 +1,5 @@
-
-
-int first = 0;
-int second = 0;
-int third = 0;
+#define DELAY 5000
+#define GLOWTIME 500
 
 int blue1 = D0;
 int green1 = D1;
@@ -16,107 +13,70 @@ int blue3 = A0;
 int green3 = A1;
 int red3 = A2;
 
+int *pins[] = {
+  &red1, &green1, &blue1,
+  &red2, &green2, &blue2,
+  &red3, &green3, &blue2
+};
+
+struct Section{
+  int isActivated;
+  int *pins[3];
+};
+Section first = {0, {&red1, &green1, &blue1}};
+Section second = {0, {&red2, &green2, &blue2}};
+Section third = {0, {&red3, &green3, &blue3}};
+
 void setup()
 {
+  for (int i = 0; i < sizeof(pins) / sizeof(int); i++) {
+    pinMode(*pins[i], OUTPUT);
+    digitalWrite(*pins[i],HIGH);
+  }
 
-    pinMode(blue1, OUTPUT);
-    pinMode(green1, OUTPUT);
-    pinMode(red1, OUTPUT);
-
-    pinMode(blue2, OUTPUT);
-    pinMode(green2, OUTPUT);
-    pinMode(red2, OUTPUT);
-
-    pinMode(blue3, OUTPUT);
-    pinMode(green3, OUTPUT);
-    pinMode(red3, OUTPUT);
-
-    Spark.function("blink",ledBlink);
-    Spark.function("thinking", thinking);
-
-
-    digitalWrite(red1,HIGH);
-    digitalWrite(green1,HIGH);
-    digitalWrite(blue1,HIGH);
-    digitalWrite(red2,HIGH);
-    digitalWrite(green2,HIGH);
-    digitalWrite(blue2,HIGH);
-    digitalWrite(red3,HIGH);
-    digitalWrite(green3,HIGH);
-    digitalWrite(blue3,HIGH);
+  Spark.function("activate", activate);
 }
 
-void turnOn(String section){
-    if (section=="first"){
-      digitalWrite(red1,LOW);
-    }
-    else if (section=="second"){
-      digitalWrite(red2,LOW);
-    }
+void turnOn(struct Section& section){
+  digitalWrite(*section.pins[0],LOW);
 }
-void turnOff(String section){
-    if (section=="first"){
-      digitalWrite(red1,HIGH);
-    }
-    else if (section=="second"){
-      digitalWrite(red2,HIGH);
-    }
+void turnOff(struct Section& section){
+  digitalWrite(*section.pins[0],HIGH);
 }
 
 void loop()
 {
-    if (first==1 || second==1 || third==1){
-      delay(5000);
-      if (first==1){
-        turnOn("first");
+    if (first.isActivated || second.isActivated || third.isActivated){
+      delay(DELAY);
+      if (first.isActivated){
+        turnOn(first);
       }
-      if (second==1){
-        turnOn("second");
+      if (second.isActivated){
+        turnOn(second);
       }
-      delay(500);
-      turnOff("first");
-      turnOff("second");
-      first = 0;
-      second = 0;
-      third = 0;
+      delay(GLOWTIME);
+      turnOff(first);
+      turnOff(second);
+      first.isActivated = 0;
+      second.isActivated = 0;
+      third.isActivated = 0;
     }
 }
 
-int thinking(String command) {
+int activate(String command) {
     if (command=="first") {
-      first = 1;
+      first.isActivated = 1;
       return 1;
     }
     else if (command=="second") {
-      second = 1;
+      second.isActivated = 1;
       return 1;
     }
     else if (command=="third") {
-      third = 1;
+      third.isActivated = 1;
       return 1;
     }
     else {
       return -1;
-    }
-}
-
-
-
-int ledBlink(String command) {
-
-    if (command=="on") {
-        digitalWrite(blue1,LOW);
-        digitalWrite(green2,LOW);
-        digitalWrite(blue3,LOW);
-        return 1;
-    }
-    else if (command=="off") {
-        digitalWrite(blue1,HIGH);
-        digitalWrite(green2,HIGH);
-        digitalWrite(blue3,HIGH);
-        return 0;
-    }
-    else {
-        return -1;
     }
 }
